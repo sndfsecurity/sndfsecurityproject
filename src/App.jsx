fb9989ddbebc5b8baa0c9fcd537af9a95b7b80a2
Developer 2 Name <sndf4315@gmail.com>
2026-04-07 17:17:48 +0530

    app code change

--- src/App.jsx
+++ src/App.jsx
@@ -21,90 +21,65 @@ import Career from "./pages/Career";
 
 import Gallery from "./pages/Media/Gallery";
 
-<<<<<<< HEAD
 /* SHOP */
 import Shop from "./pages/Shop";
 import ProductDetails from "./pages/ProductDetails";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
-=======
+
+/* SERVICES */
 import Ndf from "./pages/Services/Ndf";   
 import Sndf from "./pages/Services/Sndf"; 
 import Owl from "./pages/Services/Owl"; 
 import Spydefence from "./pages/Services/Spydefence";
 
-
->>>>>>> f91e9ff262fb705a9a8993acee94004d88908c9c
-
 function App() {
   return (
+    <>
+      <Router>
 
-    
-   <>
+        <Navbar />
 
+        <Routes>
 
-    <Router>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/course" element={<Course />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/career" element={<Career />} />
 
-      
+          {/* SHOP */}
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
 
-      <Navbar />
+          {/* LEGAL */}
+          <Route path="/privacy-policy" element={<Sndfprivacy />} />
+          <Route path="/sndfterms" element={<Sndfterms />} />
+          <Route path="/sndfdisclaimer" element={<Sndfdisclaimer />} />
 
-      <Routes>
+          {/* BRANCHES */}
+          <Route path="/branches/pune" element={<Pune />} />
+          <Route path="/branches/assam" element={<Assam />} />
 
-        
+          {/* MEDIA */}
+          <Route path="/media/gallery" element={<Gallery />} />
 
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/course" element={<Course />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/team" element={<Team />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/career" element={<Career />} />
+          {/* SERVICES */}
+          <Route path="/services/ndf" element={<Ndf />} />
+          <Route path="/services/sndf" element={<Sndf />} />
+          <Route path="/services/owl" element={<Owl />} />
+          <Route path="/services/spydefence" element={<Spydefence />} />
 
-<<<<<<< HEAD
-        {/* SHOP */}
-        <Route path="/shop" element={<Shop />} />
-<Route path="/product/:id" element={<ProductDetails />} />
-<Route path="/cart" element={<Cart />} />
-<Route path="/checkout" element={<Checkout />} />
-        {/* BRANCHES */}
-        <Route path="/branches/pune" element={<Pune />} />
-        <Route path="/branches/assam" element={<Assam />} />
+        </Routes>
 
-        {/* MEDIA */}
-        <Route path="/media/gallery" element={<Gallery />} />
-=======
-        <Route path="/privacy-policy" element={<Sndfprivacy />} />
-        <Route path="/sndfterms" element = {< Sndfterms/>}/>
-        <Route path="/sndfdisclaimer" element = {< Sndfdisclaimer/>}/>
+        <Footer />
+        <FloatingButtons />
 
-
-    
-        {/* Branch pages */}
-       <Route path="/branches/pune" element={<Pune />} />
-       <Route path="/branches/assam" element={<Assam />} />
-
-       
-       <Route path="/media/gallery" element={<Gallery />} />
-
-
-        <Route path="/services/ndf" element={<Ndf />} />
-        <Route path="/services/sndf" element={<Sndf />} />
-        <Route path="/services/owl" element={<Owl/>} />
-        <Route path="/services/spydefence" element={<Spydefence/>} />
-
-        
-
-
-
->>>>>>> f91e9ff262fb705a9a8993acee94004d88908c9c
-      </Routes>
-
-      <Footer />
-      <FloatingButtons />
-
-    </Router>
-
+      </Router>
     </>
   );
 }
