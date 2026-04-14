f89f63e2d51168ddb52c2fb5688924651a5a6ab1
Developer 2 Name <sndf4315@gmail.com>
2026-04-14 16:02:37 +0530

    code

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,6 @@ import Assam from "./pages/branches/Assam";
 
 import Career from "./pages/Career";
 
-import Gallery from "./pages/Media/Gallery";
 
 /* SHOP */
 import Shop from "./pages/Shop";
@@ -69,7 +68,6 @@ function App() {
 
 
           {/* MEDIA */}
-          <Route path="/media/gallery" element={<Gallery />} />
 
           {/* SERVICES */}
           <Route path="/services/ndf" element={<Ndf />} />
