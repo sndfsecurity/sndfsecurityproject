4108da29a289f638eb750062b4da5bdc1b897000
Developer 2 Name <sndf4315@gmail.com>
2026-04-16 15:58:16 +0530

    dev2: your work update

--- vite.config.js
+++ vite.config.js
@@ -1,41 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   plugins: [
     react(),
-    sitemap({
-      hostname: 'https://www.sndfndf.com',
 
-      // ✅ 🔥 ADD THIS LINE (MOST IMPORTANT FIX)
-      generateRobotsTxt: false,
+    
 
-      dynamicRoutes: [
-        '/',
-        '/about',
-        '/course',
-        '/contact',
-        '/team',
-        '/blog',
-        '/career',
-        '/shop',
-        '/cart',
-        '/checkout',
-        '/privacy-policy',
-        '/sndfterms',
-        '/sndfdisclaimer',
-        '/branches/pune',
-        '/branches/assam',
-        '/media/gallery',
-        '/services/ndf',
-        '/services/sndf',
-        '/services/owl',
-        '/services/spydefence'
-      ],
-
-      changefreq: 'weekly',
-      priority: 0.8
-    })
   ]
 })
